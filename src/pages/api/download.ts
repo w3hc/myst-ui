c8e2826f1b9eea385e7353ad575055fbc222f28d
Julien Béranger <julien@strat.cc>
2024-07-16 11:59:20 +0200

    Add `userAddress` (#2)
    add userAddress

--- src/pages/api/download.ts
+++ src/pages/api/download.ts
@@ -2,14 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import fetch from 'node-fetch'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { filename } = req.query
+  const { filename, userAddress } = req.query
 
   if (!filename) {
     res.status(400).json({ error: 'Filename is required' })
     return
   }
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/file/download/${filename}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/file/download/${filename}/${userAddress}`, {
     method: 'GET',
     headers: {
       'api-key': '1234',
