455129fd3b2c90bd7b020906cb5df5d54d34333e
julienbrg <julien@strat.cc>
2024-07-16 11:58:41 +0200

    add userAddress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react'
 import { Text, Button, useToast } from '@chakra-ui/react'
 import { useState } from 'react'
-import { useWeb3ModalAccount } from '@web3modal/ethers/react'
 import { LinkComponent } from '../components/layout/LinkComponent'
 import { Head } from '../components/layout/Head'
 import { SITE_NAME, SITE_DESCRIPTION } from '../utils/config'
 import axios from 'axios'
 
+import { BrowserProvider, Contract, Eip1193Provider, parseEther } from 'ethers'
+import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers/react'
+import { ERC20_CONTRACT_ADDRESS, ERC20_CONTRACT_ABI } from '../utils/erc20'
+import { HeadingComponent } from '../components/layout/HeadingComponent'
+import { ethers } from 'ethers'
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isLoadingDownload, setIsLoadingDownload] = useState<boolean>(false)
-  const [latestUpload, setLatestUpload] = useState<any>()
+  const [latestUpload, setLatestUpload] = useState<any>('file-1721123051908-173684948.txt')
 
-  const { isConnected } = useWeb3ModalAccount()
+  const { address, chainId, isConnected } = useWeb3ModalAccount()
+  const { walletProvider } = useWeb3ModalProvider()
+  const provider: Eip1193Provider | undefined = walletProvider
   const toast = useToast()
 
   const upload = async () => {
@@ -46,7 +53,7 @@ export default function Home() {
       })
     } catch (e) {
       setIsLoading(false)
-      console.log('error:', e)
+      console.log('Upload error:', e)
       toast({
         title: 'Woops',
         description: 'Something went wrong...',
@@ -63,12 +70,33 @@ export default function Home() {
     try {
       setIsLoadingDownload(true)
 
+      if (!isConnected) {
+        toast({
+          title: 'Connect wallet',
+          description: 'Please connect your wallet first.',
+          status: 'error',
+          position: 'bottom',
+          variant: 'subtle',
+          duration: 9000,
+          isClosable: true,
+        })
+        return
+      }
+
+      const ethersProvider = new BrowserProvider(provider as any)
+      const signer = await ethersProvider.getSigner()
+      console.log('signer address:', signer.address)
+
+      // const signature = await signer.signMessage('yo')
+      // console.log('signature:', signature)
+
       const filename = (await JSON.parse(latestUpload)).filename
       console.log('filename:', filename)
 
       const response = await axios.get('/api/download', {
         params: {
           filename: filename,
+          userAddress: signer.address,
         },
         responseType: 'blob',
       })
